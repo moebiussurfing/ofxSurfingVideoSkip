cf0413a64264a93b9d92450efd998350b815b33c
moebiusSurfing <moebiussurfing@gmail.com>
2022-09-21 03:14:57 -0300

    added ndi and fx pro

---
+++ examples/4_video-AdvancedFx/src/ofApp.cpp
@@ -0,0 +1,16 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+	player.setup();
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+	player.windowResized(w, h);
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+	player.dragEvent(dragInfo);
+}
